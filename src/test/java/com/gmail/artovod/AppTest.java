404d68c1be2519d1d65b28dc00240cd9b1a29b2a
Artem Ovodov <aovodov@qatools.ru>
2025-09-19 16:14:46 +0300

    add test

--- src/test/java/com/gmail/artovod/AppTest.java
+++ src/test/java/com/gmail/artovod/AppTest.java
@@ -1,14 +1,11 @@
 package com.gmail.artovod;
 
-import io.qameta.allure.junit4.DisplayName;
-import org.junit.Test;
-
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Assertions;
+import io.qameta.allure.*;
 import static io.qameta.allure.Allure.parameter;
 import static io.qameta.allure.Allure.step;
-import static org.junit.Assert.*;
-import io.qameta.allure.*;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -32,9 +29,9 @@ public class AppTest
     @Severity(SeverityLevel.CRITICAL)
     @Owner("artemovodov")
     public void testAdd() {
-        assertEquals(5.0, calculator.add(2.0, 3.0), 0.001);
-        assertEquals(0.0, calculator.add(-2.0, 2.0), 0.001);
-        assertEquals(-5.0, calculator.add(-2.0, -3.0), 0.001);
+        Assertions.assertEquals(5.0, calculator.add(2.0, 3.0), 0.001);
+        Assertions.assertEquals(0.0, calculator.add(-2.0, 2.0), 0.001);
+        Assertions.assertEquals(-5.0, calculator.add(-2.0, -3.0), 0.001);
     }
 
     @Test
@@ -43,9 +40,9 @@ public class AppTest
     @Severity(SeverityLevel.NORMAL)
     @Owner("artemovodov")
     public void testSubtract() {
-        assertEquals(-1.0, calculator.subtract(2.0, 3.0), 0.001);
-        assertEquals(5.0, calculator.subtract(2.0, -3.0), 0.001);
-        assertEquals(0.0, calculator.subtract(2.0, 2.0), 0.001);
+        Assertions.assertEquals(-1.0, calculator.subtract(2.0, 3.0), 0.001);
+        Assertions.assertEquals(5.0, calculator.subtract(2.0, -3.0), 0.001);
+        Assertions.assertEquals(0.0, calculator.subtract(2.0, 2.0), 0.001);
     }
 
     @Test
@@ -54,9 +51,9 @@ public class AppTest
     @Severity(SeverityLevel.NORMAL)
     @Owner("artemovodov")
     public void testMultiply() {
-        assertEquals(6.0, calculator.multiply(2.0, 3.0), 0.001);
-        assertEquals(-6.0, calculator.multiply(-2.0, 3.0), 0.001);
-        assertEquals(6.0, calculator.multiply(-2.0, -3.0), 0.001);
+        Assertions.assertEquals(6.0, calculator.multiply(2.0, 3.0), 0.001);
+        Assertions.assertEquals(-6.0, calculator.multiply(-2.0, 3.0), 0.001);
+        Assertions.assertEquals(6.0, calculator.multiply(-2.0, -3.0), 0.001);
     }
 
     @Test
@@ -65,18 +62,20 @@ public class AppTest
     @Severity(SeverityLevel.NORMAL)
     @Owner("artemovodov")
     public void testDivide() {
-        assertEquals(2.0, calculator.divide(6.0, 3.0), 0.001);
-        assertEquals(-2.0, calculator.divide(-6.0, 3.0), 0.001);
-        assertEquals(2.0, calculator.divide(-6.0, -3.0), 0.001);
+        Assertions.assertEquals(2.0, calculator.divide(6.0, 3.0), 0.001);
+        Assertions.assertEquals(-2.0, calculator.divide(-6.0, 3.0), 0.001);
+        Assertions.assertEquals(2.0, calculator.divide(-6.0, -3.0), 0.001);
     }
 
-    @Test(expected = ArithmeticException.class)
+    @Test
     @Story("Деление на ноль")
     @Description("Проверка деления на ноль")
     @Severity(SeverityLevel.BLOCKER)
     @Owner("artemovodov")
     public void testDivideByZero() {
-        calculator.divide(6.0, 0.0);
+        Assertions.assertThrows(ArithmeticException.class, () -> {
+            calculator.divide(6.0, 0.0);
+        });
     }
 
     @Test
